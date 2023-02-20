4f389391a52f0e39f47864355a98933d1a9fc60e
Yunus Emre <yemregul94@gmail.com>
2023-02-20 15:39:42 +0300

    minor listing fix

--- app/src/main/java/com/moonlight/nobetcieczaneler/ui/pharmacylist/PharmacyListFragment.kt
+++ app/src/main/java/com/moonlight/nobetcieczaneler/ui/pharmacylist/PharmacyListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -35,13 +34,12 @@ class PharmacyListFragment : Fragment() {
     ): View {
         _binding = FragmentPharmacyListBinding.inflate(inflater, container, false)
 
+        loadList()
         checkListType()
 
         binding.layoutMap.setOnClickListener {
             loadMap()
         }
-
-        loadList()
 
         return binding.root
     }
